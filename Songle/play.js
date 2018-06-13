0ea8d2d6caffaf9aa4dcdfda7868c772bb999668
theAwesomeKoara <KoaraKoara9663@gmail.com>
2018-06-13 17:46:14 +0900

    songle2
    hehehe

--- Songle/play.js
+++ Songle/play.js
@@ -29,45 +29,21 @@ let combo=0;
 window.onload=function(){
   let songleWidgetElement=SongleWidgetAPI.createSongleWidgetElement({
     api: "unchi",
-    url: getParams()['url']
+    url: "www.youtube.com/watch?v=5ptnDFsmSGk"
   });
 
   document.getElementById("widget").appendChild(songleWidgetElement);
 }
 
-//--
+window.onSongleWidgetReady=function(_api,_songleWidget){
+  songleWidget=_songleWidget;
+  songleWidget.play();
 
-document.addEventListener("deviceready",function(){
-  let useTileIds=JSON.parse(window.localStorage.getItem("useTileIds"));
-  for (tileId of useTileIds) {
-    let tile={
-      id: tileId,
-      topNote: null
-    };
-    tiles.push(tile);
-  }
+  generateNotes();
 
-  cordova.plugins.moto.setAllTilesColor("off",false,false);
-  cordova.plugins.moto.setAntPressEvent(function(tileId){
-    //タイルを踏んだ際毎回判定を行う
-    let tile=tiles.find(tile=>tile.id==tileId);
-    if(tile!=undefined){
-      doJudgeOf(tile);
-    }
-  },false);
-
-  //--
-
-  window.onSongleWidgetReady=function(_api,_songleWidget){
-    songleWidget=_songleWidget;
-    songleWidget.play();
-
-    generateNotes();
-
-    //ループ
-    setInterval(function(){
-      if(songleWidget.position!=null)position=songleWidget.position;
-      stepNotesAndTiles();
-    },1);
-  }
-});
+  //ループ
+  setInterval(function(){
+    if(songleWidget.position!=null)position=songleWidget.position;
+    stepNotesAndTiles();
+  },1);
+}
